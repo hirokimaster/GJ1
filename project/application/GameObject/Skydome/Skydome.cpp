d9da0313a584b1b261244dabe606b33e74ad41c6
OikawaYuma <k022g0016@g.neec.ac.jp>
2025-09-03 13:16:41 +0900

    よろしくお願いします

---
+++ project/application/GameObject/Skydome/Skydome.cpp
@@ -0,0 +1,13 @@
+#include "Skydome.h"
+#include "engine/3d/ObjectManager/ObjectManager.h"
+#include "engine/3d/Model/ModelManager.h"
+#include "engine/Graphics/TextureManager/TextureManager.h"
+
+
+void Skydome::Init()
+{
+}
+
+void Skydome::Update()
+{
+}
---
+++ project/application/GameObject/Skydome/Skydome.h
@@ -0,0 +1,8 @@
+#pragma once
+class Skydome
+{
+public:
+	void Init();
+	void Update();
+};
+
