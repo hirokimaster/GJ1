8863dd57d2a7a6234910054009bee35b0d4f0ec2
OikawaYuma <k022g0016@g.neec.ac.jp>
2025-09-03 14:06:24 +0900

    スカイドームは出せた

--- project/application/GameObject/Skydome/Skydome.cpp
+++ project/application/GameObject/Skydome/Skydome.cpp
@@ -6,8 +6,18 @@
 
 void Skydome::Init()
 {
+	TextureManager::Load("resources/TempTexture/white2.png");
+	ModelManager::LoadObjModel("Skydome/skydome.obj");
+	// object共通の初期化
+	// object共通の初期化
+	BaseIndividualObject::Initialize("Skydome/skydome.obj", "TempTexture/white2.png", ColliderType::AABB);
+	object_.lock()->SetEnableLighting(false);
+	object_.lock()->SetScale({ 150.0f,150.0f,150.0f });
+	object_.lock()->SetColor({0.6f,0.6f,0.6f,1.0f});
+
 }
 
 void Skydome::Update()
 {
+	BaseIndividualObject::Update(); // object共通の更新処理
 }
--- project/application/GameObject/Skydome/Skydome.h
+++ project/application/GameObject/Skydome/Skydome.h
@@ -1,8 +1,11 @@
 #pragma once
-class Skydome
+#include <memory>
+#include "engine/3d/BaseObject/BaseIndividualObject.h"
+class Skydome : public BaseIndividualObject
 {
 public:
 	void Init();
 	void Update();
+private:
 };
 
